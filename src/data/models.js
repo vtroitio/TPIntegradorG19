0c73750869a1078183a1119e02307be7385f0720
vtroitio <valentinbtroitino@gmail.com>
2024-05-07 17:44:19 -0300

    Estructura base de la pagina

---
+++ src/data/models.js
@@ -0,0 +1,14 @@
+export const models = [
+    {
+        path: '/models/Flamingo.glb',
+        distance: 250,
+    },
+    {
+        path: '/models/retro_computer_setup_free.glb',
+        distance: 200,
+    },
+    {
+        path: '/models/Horse.glb',
+        distance: 550,
+    }
+]
--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,6 @@
-// Import our custom CSS
-import '../scss/styles.scss';
+import * as bootstrap from 'bootstrap';
+import { models } from '../data/models.js';
+import Product from './Product.js';
 
-// Import only the Bootstrap components we need
-import { Popover } from 'bootstrap';
-
-// Add validation for the contact form
-document.addEventListener("DOMContentLoaded", function() {
-    // Agregar evento 'submit' al formulario
-    document.getElementById("contactForm").addEventListener("submit", function(event) {
-        // Obtener los valores de los campos
-        var name = document.getElementById("name").value;
-        var email = document.getElementById("email").value;
-        var message = document.getElementById("message").value;
-        var howFound = document.querySelectorAll('input[name="howFound"]:checked');
-        var reason = document.getElementById("selectOption").value;
-
-        // Validar que los campos obligatorios estén completos
-        if (!name || !email || !message || howFound.length === 0 || !reason) {
-            // Mostrar un mensaje de error
-            alert("Por favor completa todos los campos obligatorios.");
-            // Detener el envío del formulario
-            event.preventDefault();
-        }
-    });
-
-    // Create an example popover
-    document.querySelectorAll('[data-bs-toggle="popover"]').forEach(popover => {
-        new Popover(popover);
-    });
-});
+const canvases = document.querySelectorAll('.product-canvas');
+canvases.forEach((canvas, index) => new Product(canvas, models[index]));
